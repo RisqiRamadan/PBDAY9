448e9132c5a692d6432f1879fa2ff0c2bb982205
RisqiRamadan <rrisqi988@gmail.com>
2024-05-08 21:29:27 +0700

    Done Alhamdulillah

--- app/src/main/java/com/example/pbday9/LoginActivity.java
+++ app/src/main/java/com/example/pbday9/LoginActivity.java
@@ -4,30 +4,73 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
+import com.example.pbday9.data.ApiClient;
+import com.example.pbday9.data.ApiInterface;
+import com.example.pbday9.data.model.Login;
+import com.example.pbday9.data.model.LoginData;
+import com.example.pbday9.data.sessionManager;
 import com.example.pbday9.databinding.ActivityLoginBinding;
+import com.google.android.material.snackbar.Snackbar;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+    private String Username;
+    private String Password;
+    private sessionManager sessionmanager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.tvCNAccount.setOnClickListener(this);
+        binding.tvCNAccount.setOnClickListener(v -> {
+            if (v.getId() == R.id.tvCNAccount){
+                Intent intent = new Intent(this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.btnlogin.setOnClickListener(v -> {
+            Username = binding.etLoginL.getText().toString();
+            Password = binding.etRegisterL.getText().toString();
+            login(Username, Password);
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.tvCNAccount) {
-            Intent intent = new Intent (this, RegisterActivity.class);
-            startActivity(intent);
-        }
+    public void login(String Username, String Password){
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<Login> call = apiService.LoginResponse(Username,Password);
+        call.enqueue(new Callback<Login>(){
+
+            @Override
+            public void onResponse(Call<Login> call, Response<Login> response) {
+                if (response.isSuccessful() && response.body() != null && response.body().isStatus()){
+
+                    sessionmanager = new sessionManager(LoginActivity.this);
+                    LoginData loginData = response.body().getData();
+                    sessionmanager.loginSession(loginData);
+
+                    Snackbar.make(findViewById(android.R.id.content), "Berhasil Login", Snackbar.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Snackbar.make(findViewById(android.R.id.content), "Gagal Login : " + response.message(), Snackbar.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Login> call, Throwable t) {
+                Snackbar.make(findViewById(android.R.id.content), "Gagal Login : " + t.getMessage(), Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pbday9/MainActivity.java
+++ app/src/main/java/com/example/pbday9/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.pbday9;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import com.example.pbday9.data.sessionManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    sessionManager sessionmanager ;
+    TextView tvname, tvusername;
+    String username,name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvusername = findViewById(R.id.tvusername);
+        tvname = findViewById(R.id.tvnama);
+
+        sessionmanager = new sessionManager(this);
+        if (!sessionmanager.isLogIng()){
+            goLogin();
+        }
+
+        username = sessionmanager.getUser().get(sessionManager.USERNAME);
+        name = sessionmanager.getUser().get(sessionManager.NAME);
+
+        tvusername.setText(username);
+        tvname.setText(name);
+    }
+
+    private void goLogin() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout){
+            sessionmanager.logout();
+            goLogin();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
